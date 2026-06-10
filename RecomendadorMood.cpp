0aa4e38583511b99a10627af273919c22111715a
gabriela-1130 <a01714118@tec.mx>
2026-06-09 18:13:37 -0600

    Update RecomendadorMood.cpp

--- RecomendadorMood.cpp
+++ RecomendadorMood.cpp
@@ -1,21 +1,44 @@
-
 #include "RecomendadorMood.h"
 
-Playlist RecomendadorMood::generarPlaylist() {
-
-    Playlist p(
-        "Playlist Emocional",
-        "Melancolico"
-    );
-
-    cout << "Generando playlist basada en mood..." << endl;
-
-    return p;
+RecomendadorMood::RecomendadorMood() {
+    moodSeleccionado = "";
 }
 
-vector<Cancion*> RecomendadorMood::recomendarCanciones() {
+vector<Cancion*> RecomendadorMood::recomendarCanciones(Biblioteca* biblioteca) {
 
-    vector<Cancion*> canciones;
+    return biblioteca->buscarMood(moodSeleccionado);
+}
 
-    return canciones;
+Playlist RecomendadorMood::generarPlaylist(Biblioteca* biblioteca) {
+
+    int opcion;
+
+    cout << endl;
+    cout << "===== RECOMENDADOR POR MOOD =====" << endl;
+    cout << "1. Feliz" << endl;
+    cout << "2. Triste" << endl;
+    cout << "3. Melancolico" << endl;
+    cout << "4. Energetico" << endl;
+    cout << "5. Relajado" << endl;
+    cout << "Selecciona una opcion: ";
+    cin >> opcion;
+
+    if (opcion == 1) moodSeleccionado = "Feliz";
+    else if (opcion == 2) moodSeleccionado = "Triste";
+    else if (opcion == 3) moodSeleccionado = "Melancolico";
+    else if (opcion == 4) moodSeleccionado = "Energetico";
+    else moodSeleccionado = "Relajado";
+
+    Playlist p(
+        "Playlist " + moodSeleccionado,
+        moodSeleccionado
+    );
+
+    p.agregarCancion(
+        recomendarCanciones(biblioteca)
+    );
+
+    historialRecomendaciones.push_back(p);
+
+    return p;
 }
