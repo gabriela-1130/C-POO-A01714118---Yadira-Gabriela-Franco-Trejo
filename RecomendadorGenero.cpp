c36839a7998b84b3618442b211f3180abb2bd024
gabriela-1130 <a01714118@tec.mx>
2026-06-09 18:13:10 -0600

    Update RecomendadorGenero.cpp

--- RecomendadorGenero.cpp
+++ RecomendadorGenero.cpp
@@ -1,21 +1,42 @@
-
 #include "RecomendadorGenero.h"
 
-Playlist RecomendadorGenero::generarPlaylist() {
-
-    Playlist p(
-        "Playlist de Genero",
-        "Rock"
-    );
-
-    cout << "Generando playlist basada en genero..." << endl;
-
-    return p;
+RecomendadorGenero::RecomendadorGenero() {
+    generoSeleccionado = "";
 }
 
-vector<Cancion*> RecomendadorGenero::recomendarCanciones() {
+vector<Cancion*> RecomendadorGenero::recomendarCanciones(Biblioteca* biblioteca) {
 
-    vector<Cancion*> canciones;
+    return biblioteca->buscarGenero(generoSeleccionado);
+}
 
-    return canciones;
+Playlist RecomendadorGenero::generarPlaylist(Biblioteca* biblioteca) {
+
+    int opcion;
+
+    cout << endl;
+    cout << "===== RECOMENDADOR POR GENERO =====" << endl;
+    cout << "1. Rock" << endl;
+    cout << "2. Pop" << endl;
+    cout << "3. Indie" << endl;
+    cout << "4. Metal" << endl;
+    cout << "Selecciona una opcion: ";
+    cin >> opcion;
+
+    if (opcion == 1) generoSeleccionado = "Rock";
+    else if (opcion == 2) generoSeleccionado = "Pop";
+    else if (opcion == 3) generoSeleccionado = "Indie";
+    else generoSeleccionado = "Metal";
+
+    Playlist p(
+        "Playlist " + generoSeleccionado,
+        generoSeleccionado
+    );
+
+    p.agregarCancion(
+        recomendarCanciones(biblioteca)
+    );
+
+    historialRecomendaciones.push_back(p);
+
+    return p;
 }
