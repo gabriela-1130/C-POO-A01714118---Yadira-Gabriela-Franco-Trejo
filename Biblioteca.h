7635391d9668d625ae97aaa0599894663850f676
gabriela-1130 <a01714118@tec.mx>
2026-06-09 18:06:52 -0600

    Update Biblioteca.h

--- Biblioteca.h
+++ Biblioteca.h
@@ -1,20 +1,37 @@
 #ifndef BIBLIOTECA_H
 #define BIBLIOTECA_H
 
+#include <iostream>
 #include <vector>
+#include <string>
+
 #include "Cancion.h"
 #include "Playlist.h"
+
+using namespace std;
 
 class Biblioteca {
 
 private:
-    vector<Cancion*> catalogoCanciones;
-    vector<Playlist> playlistsDisponibles;
+    vector<Cancion*> catalogo;
+    vector<Playlist> playlistDisponibles;
 
 public:
-    void buscarCancion();
-    void buscarGenero();
-    void buscarMood();
+    Biblioteca();
+    ~Biblioteca();
+
+    void agregarCancion(Cancion* cancion);
+    void agregarPlaylist(Playlist playlist);
+
+    void mostrarCatalogo();
+
+    Cancion* buscarCancion(string nombre);
+    vector<Cancion*> buscarGenero(string genero);
+    vector<Cancion*> buscarMood(string mood);
+
+    Cancion* obtenerCancion(int indice);
+
+    void cargarCancionesDesdeArchivo(string nombreArchivo);
 };
 
 #endif
