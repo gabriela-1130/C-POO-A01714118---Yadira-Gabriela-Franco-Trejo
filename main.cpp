8a6869bcd2498d86f6b051c29979af7a332184e8
gabriela-1130 <a01714118@tec.mx>
2026-06-09 18:15:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-//Yadira Gabriela Franco Trejo A01714118
+// Yadira Gabriela Franco Trejo A01714118
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
-
 
 #include "Cancion.h"
 #include "CancionRock.h"
@@ -13,20 +13,14 @@ using namespace std;
 #include "CancionMetal.h"
 
 #include "Playlist.h"
-
 #include "Usuario.h"
+#include "Historial.h"
+#include "Biblioteca.h"
 
 #include "Recomendador.h"
 #include "RecomendadorMood.h"
 #include "RecomendadorGenero.h"
 #include "RecomendadorEnergia.h"
-
-#include "Biblioteca.h"
-#include "Historial.h"
-
-
-// IMPLEMENTACIONES
-
 
 #include "Cancion.cpp"
 #include "CancionRock.cpp"
@@ -35,171 +29,369 @@ using namespace std;
 #include "CancionMetal.cpp"
 
 #include "Playlist.cpp"
-
 #include "Usuario.cpp"
+#include "Historial.cpp"
+#include "Biblioteca.cpp"
 
 #include "Recomendador.cpp"
 #include "RecomendadorMood.cpp"
 #include "RecomendadorGenero.cpp"
 #include "RecomendadorEnergia.cpp"
 
-#include "Biblioteca.cpp"
-#include "Historial.cpp"
-
-void probarPolimorfismoCanciones(vector<Cancion*> canciones) {
-
-    cout << "POLIMORFISMO EN CANCIONES" << endl;
+void mostrarMenu() {
     cout << endl;
+    cout << "------------------------------------" << endl;
+    cout << " SISTEMA DE RECOMENDACION MUSICAL" << endl;
+    cout << "------------------------------------" << endl;
+    cout << "1. Mostrar catalogo" << endl;
+    cout << "2. Buscar cancion por titulo" << endl;
+    cout << "3. Escuchar cancion" << endl;
+    cout << "4. Crear playlist" << endl;
+    cout << "5. Ver mis playlists" << endl;
+    cout << "6. Agregar cancion a playlist" << endl;
+    cout << "7. Eliminar cancion de playlist" << endl;
+    cout << "8. Ver historial" << endl;
+    cout << "9. Recomendar musica" << endl;
+    cout << "10. Ver perfil" << endl;
+    cout << "11. Salir" << endl;
+    cout << "Opcion: ";
+}
 
-    for (Cancion* c : canciones) {
+void mostrarPlaylists(vector<Playlist>& playlists) {
+    if (playlists.size() == 0) {
+        cout << "No tienes playlists creadas." << endl;
+        return;
+    }
 
-        c->reproducir();
+    cout << endl;
+    cout << "----- MIS PLAYLISTS -----" << endl;
 
-        c->mostrarInfo();
-
-        cout << endl;
+    for (int i = 0; i < playlists.size(); i++) {
+        cout << i + 1 << ". " << playlists[i].getNombre()
+             << " (" << playlists[i].getMoodPrincipal() << ")" << endl;
     }
 }
 
-void probarPolimorfismoRecomendadores(
-    vector<Recomendador*> recomendadores
-) {
+int elegirPlaylist(vector<Playlist>& playlists) {
+    if (playlists.size() == 0) {
+        cout << "Primero debes crear una playlist." << endl;
+        return -1;
+    }
 
-    cout << "POLIMORFISMO EN RECOMENDADORES" << endl;
+    mostrarPlaylists(playlists);
+
+    int indice;
+    cout << "Elige el numero de playlist: ";
+    cin >> indice;
+
+    if (indice < 1 || indice > (int)playlists.size()) {
+        cout << "Indice invalido." << endl;
+        return -1;
+    }
+
+    return indice - 1;
+}
+
+void guardarPlaylistRecomendada(vector<Playlist>& playlists, Playlist playlist) {
+    int opcion;
+
     cout << endl;
+    cout << "Quieres guardar esta playlist?" << endl;
+    cout << "1. Si" << endl;
+    cout << "2. No" << endl;
+    cout << "Opcion: ";
+    cin >> opcion;
 
-    for (Recomendador* r : recomendadores) {
-
-        Playlist p = r->generarPlaylist();
-
-        p.mostrarPlaylist();
-
-        cout << endl;
+    if (opcion == 1) {
+        playlists.push_back(playlist);
+        cout << "Playlist guardada correctamente." << endl;
+    }
+    else {
+        cout << "Playlist no guardada." << endl;
     }
 }
 
 int main() {
 
-    Cancion* c1 = new CancionRock(
-        "Toxicity",
-        "System of a Down",
-        "Metal",
-        "Energetico",
-        9,
-        3.5
-    );
+    Biblioteca biblioteca;
 
-    Cancion* c2 = new CancionPop(
-        "Blinding Lights",
-        "The Weeknd",
-        "Pop",
-        "Feliz",
-        8,
-        3.2
-    );
+    biblioteca.cargarCancionesDesdeArchivo("canciones.csv");
 
-    Cancion* c3 = new CancionIndie(
-        "Apocalypse",
-        "Cigarettes After Sex",
-        "Indie",
-        "Melancolico",
-        4,
-        4.5
-    );
+    string nombreUsuario;
+    int edad;
+    string moodActual;
+    int energia;
+    string generoFavorito;
+    int opcionGenero;
 
-    Cancion* c4 = new CancionMetal(
-        "Master of Puppets",
-        "Metallica",
-        "Metal",
-        "Energetico",
-        10,
-        8.3
-    );
+    cout << "----- CREAR PERFIL -----" << endl;
 
+    cout << "Nombre: ";
+    getline(cin, nombreUsuario);
 
-    vector<Cancion*> todasLasCanciones;
+    cout << "Edad: ";
+    cin >> edad;
+    cin.ignore();
 
-    todasLasCanciones.push_back(c1);
-    todasLasCanciones.push_back(c2);
-    todasLasCanciones.push_back(c3);
-    todasLasCanciones.push_back(c4);
+    cout << "Mood actual: ";
+    getline(cin, moodActual);
 
-    probarPolimorfismoCanciones(todasLasCanciones);
+    cout << "Nivel de energia (1-10): ";
+    cin >> energia;
+    if (energia < 1) {
+    energia = 1;
+    }
+    else if (energia > 10) {
+        energia = 10;
+    }
+    cin.ignore();
+    
 
-//playlist
-
-    Playlist p1(
-        "Noches Tristes",
-        "Melancolico"
-    );
-
-//Sobrecarga 
-    // Agregar UNA cancion
-    p1.agregarCancion(c1);
-
-    //Varias canciones
-    vector<Cancion*> canciones;
-
-    canciones.push_back(c2);
-    canciones.push_back(c3);
-    canciones.push_back(c4);
-
-    p1.agregarCancion(canciones);
-
-    //playlist 
-    cout << endl;
-
-    p1.mostrarPlaylist();
-
-
-    //Clases colaborando entre sí
-    Usuario u1(
-    "Gaby",
-    17,
-    "Melancolico",
-    4,
-    "Estudiar",
-    "Indie"
-    );
-
-    u1.iniciarSesion();
-
-    u1.escucharCancion(c3);
 
     cout << endl;
+    cout << "Genero favorito:" << endl;
+    cout << "1. Rock" << endl;
+    cout << "2. Pop" << endl;
+    cout << "3. Indie" << endl;
+    cout << "4. Metal" << endl;
+    cout << "Opcion: ";
+    cin >> opcionGenero;
+    cin.ignore();
 
-    u1.verHistorial();
+    if (opcionGenero == 1) {
+        generoFavorito = "Rock";
+    }
+    else if (opcionGenero == 2) {
+        generoFavorito = "Pop";
+    }
+    else if (opcionGenero == 3) {
+        generoFavorito = "Indie";
+    }
+    else if (opcionGenero == 4) {
+        generoFavorito = "Metal";
+    }
+    else {
+        generoFavorito = "Rock";
+    }
 
-    //Crear recomendadores
-    cout << endl;
-    cout << "RECOMENDADORES" << endl;
-    cout << endl;
+    Usuario usuario(
+        nombreUsuario,
+        edad,
+        moodActual,
+        energia,
+        generoFavorito
+    );
 
-    Recomendador* r1 = new RecomendadorMood();
+    usuario.iniciarSesion();
 
-    Recomendador* r2 = new RecomendadorGenero();
+    vector<Playlist> misPlaylists;
 
-    Recomendador* r3 = new RecomendadorEnergia();
+    int opcion = 0;
 
-   vector<Recomendador*> recomendadores;
+    while (opcion != 11)  {
 
-    recomendadores.push_back(r1);
-    recomendadores.push_back(r2);
-    recomendadores.push_back(r3);
+        mostrarMenu();
+        cin >> opcion;
+        cin.ignore();
 
-    cout << endl;
+        if (opcion == 1) {
 
-    probarPolimorfismoRecomendadores(recomendadores);
+            biblioteca.mostrarCatalogo();
+        }
 
-    //Liberar memoria
-    delete c1;
-    delete c2;
-    delete c3;
-    delete c4;
+        else if (opcion == 2) {
 
-    delete r1;
-    delete r2;
-    delete r3;
+            string titulo;
+
+            cout << "Escribe el titulo exacto de la cancion: ";
+            getline(cin, titulo);
+
+            Cancion* encontrada = biblioteca.buscarCancion(titulo);
+
+            if (encontrada != nullptr) {
+                cout << "Cancion encontrada:" << endl;
+                encontrada->mostrarInfo();
+
+                int escuchar;
+                cout << "Quieres escuchar esta cancion?" << endl;
+                cout << "1. Si" << endl;
+                cout << "2. No" << endl;
+                cout << "Opcion: ";
+                cin >> escuchar;
+
+                if (escuchar == 1) {
+                    usuario.escucharCancion(encontrada);
+                }
+
+                cin.ignore();
+            }
+            else {
+                cout << "No se encontro la cancion." << endl;
+            }
+        }
+
+        else if (opcion == 3) {
+
+            int indice;
+
+            biblioteca.mostrarCatalogo();
+
+            cout << "Elige el numero de la cancion que quieres escuchar: ";
+            cin >> indice;
+
+            Cancion* cancion = biblioteca.obtenerCancion(indice);
+
+            usuario.escucharCancion(cancion);
+        }
+
+        else if (opcion == 4) {
+
+            string nombrePlaylist;
+            string moodPlaylist;
+
+            cout << "Nombre de la playlist: ";
+            getline(cin, nombrePlaylist);
+
+            cout << "Mood principal de la playlist: ";
+            getline(cin, moodPlaylist);
+
+            Playlist nueva = usuario.crearPlaylist(
+                nombrePlaylist,
+                moodPlaylist
+            );
+
+            misPlaylists.push_back(nueva);
+
+            biblioteca.agregarPlaylist(nueva);
+        }
+
+        else if (opcion == 5) {
+
+            mostrarPlaylists(misPlaylists);
+
+            if (misPlaylists.size() > 0) {
+                int ver;
+
+                cout << "Quieres ver el contenido de alguna playlist?" << endl;
+                cout << "1. Si" << endl;
+                cout << "2. No" << endl;
+                cout << "Opcion: ";
+                cin >> ver;
+
+                if (ver == 1) {
+                    int indicePlaylist = elegirPlaylist(misPlaylists);
+
+                    if (indicePlaylist != -1) {
+                        misPlaylists[indicePlaylist].mostrarPlaylist();
+                    }
+                }
+            }
+        }
+
+        else if (opcion == 6) {
+
+            int indicePlaylist = elegirPlaylist(misPlaylists);
+
+            if (indicePlaylist != -1) {
+
+                int indiceCancion;
+
+                biblioteca.mostrarCatalogo();
+
+                cout << "Elige el numero de la cancion que quieres agregar: ";
+                cin >> indiceCancion;
+
+                Cancion* cancion = biblioteca.obtenerCancion(indiceCancion);
+
+                if (cancion != nullptr) {
+                    misPlaylists[indicePlaylist].agregarCancion(cancion);
+                    cout << "Cancion agregada correctamente." << endl;
+                }
+                else {
+                    cout << "Cancion invalida." << endl;
+                }
+            }
+        }
+
+        else if (opcion == 7) {
+
+            int indicePlaylist = elegirPlaylist(misPlaylists);
+
+            if (indicePlaylist != -1) {
+                misPlaylists[indicePlaylist].mostrarPlaylist();
+
+                int indiceCancion;
+
+                cout << "Elige el numero de la cancion a eliminar: ";
+                cin >> indiceCancion;
+
+                misPlaylists[indicePlaylist].eliminarCancion(indiceCancion);
+            }
+        }
+
+        else if (opcion == 8) {
+
+            usuario.verHistorial();
+        }
+
+        else if (opcion == 9) {
+
+            int tipo;
+
+            cout << endl;
+            cout << "----- RECOMENDADOR MUSICAL -----" << endl;
+            cout << "1. Recomendacion por mood" << endl;
+            cout << "2. Recomendacion por genero" << endl;
+            cout << "3. Recomendacion por energia" << endl;
+            cout << "Opcion: ";
+            cin >> tipo;
+
+            Recomendador* recomendador = nullptr;
+
+            if (tipo == 1) {
+                recomendador = new RecomendadorMood();
+            }
+            else if (tipo == 2) {
+                recomendador = new RecomendadorGenero();
+            }
+            else if (tipo == 3) {
+                recomendador = new RecomendadorEnergia();
+            }
+            else {
+                cout << "Opcion invalida." << endl;
+            }
+
+            if (recomendador != nullptr) {
+
+                Playlist recomendada =
+                    recomendador->generarPlaylist(&biblioteca);
+
+                recomendada.mostrarPlaylist();
+
+                guardarPlaylistRecomendada(
+                    misPlaylists,
+                    recomendada
+                );
+
+                delete recomendador;
+            }
+        }
+
+        else if (opcion == 10) {
+
+            usuario.mostrarPerfil();
+        }
+
+        else if (opcion == 11) {
+
+            cout << "Saliendo del sistema..." << endl;
+        }
+
+        else {
+
+            cout << "Opcion invalida." << endl;
+        }
+    }
 
     return 0;
 }
